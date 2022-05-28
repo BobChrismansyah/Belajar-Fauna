fcc8827413f1fe1197d5590f060d2b613b38f2bf
bob <kabangabcp20h@student.unhas.ac.id>
2022-05-28 22:13:38 +0800

    11

---
+++ app/src/main/java/com/example/belajarfauna/content_main.java
@@ -0,0 +1,14 @@
+package com.example.belajarfauna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class content_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_main);
+    }
+}
---
+++ app/src/main/java/com/example/belajarfauna/loading_screen.java
@@ -0,0 +1,37 @@
+package com.example.belajarfauna;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class loading_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        Thread loading = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(loading_screen.this, content_main.class));
+                }
+            }
+        };
+        loading.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
