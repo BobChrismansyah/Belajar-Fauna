68cb108d52eb842f3c3f1b66c44ae4f335c09f3f
bob <kabangabcp20h@student.unhas.ac.id>
2022-05-29 00:35:12 +0800

    11

--- app/src/main/java/com/example/belajarfauna/content_main.java
+++ app/src/main/java/com/example/belajarfauna/content_main.java
@@ -1,14 +1,31 @@
 package com.example.belajarfauna;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class content_main extends AppCompatActivity {
+    private CardView darat, air, amphibi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_main);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        darat = (CardView) findViewById(R.id.content_darat);
+        air = (CardView) findViewById(R.id.content_air);
+        amphibi = (CardView) findViewById(R.id.content_amphibi);
+
+        darat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(content_main.this, hewandarat.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/belajarfauna/hewandarat.java
@@ -0,0 +1,16 @@
+package com.example.belajarfauna;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class hewandarat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hewandarat);
+
+    }
+}
